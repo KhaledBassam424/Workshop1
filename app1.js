dddd39cdccfc015778b2f732b4a3842fe6ba9431
KhaledBassam424 <khaleddardour427@gmail.com>
2021-01-28 08:25:27 +0200

    if

--- app1.js
+++ app1.js
@@ -1,13 +1,14 @@
 console.log('khaled') ;
 20 == '20' ;
 20 != '20'
-var newUser = 'mohammad'
-alert('HI ' + newUser)
 var mohammed;
 mohammed = 3;
 var userName = prompt('what is your name')
-alert('HI' + userName)
-
+alert('HI ' + userName)
+var x = 20 
+var y = 40 
+var z = prompt('pick a number')
+if(z>(x+y)){alert('you won');}else if(z<(x+y)){alert('you lost');} else{alert(nothing);}
 
 
 
