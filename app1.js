906a755291d9cd47091b003cad5c2aa4e9f4f5a8
KhaledBassam424 <khaleddardour427@gmail.com>
2021-01-28 08:08:05 +0200

    cha

--- app1.js
+++ app1.js
@@ -1,3 +1,13 @@
+console.log('khaled') ;
+20 == '20' ;
+20 != '20'
+var newUser = 'mohammad'
+alert('HI ' + newUser)
+var mohammed;
+mohammed = 3;
+var userName = prompt('what is your name')
+alert('HI' + userName)
+
 
 
 
