a8a46be0167b6c24ab18dbbf344c545e00135916
KhaledBassam424 <khaleddardour427@gmail.com>
2021-01-29 15:06:21 +0200

    reactivate

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 //console.log('khaled')
 
-//var userInput = prompt('what is your name') ;
-//alert('HI ' + userInput);
+var userInput = prompt('what is your name') ;
+alert('HI ' + userInput);
 
-//var yourWeight = prompt('what is your weight');
-//if(yourWeight>70){document.write('reduce your weight');} else{alert('you are healthy');}
+var yourWeight = prompt('what is your weight');
+if(yourWeight>70){document.write('reduce your weight');} else{alert('you are healthy');}
 
-//var x = 5
-//var y = 10
-//var z = prompt('pick a number') ;
-//if(z>(x+y)){alert('you won');}
-//else if(z<(x+y)){alert('you lost');} else{alert('nothing');} 
+var x = 5
+var y = 10
+var z = prompt('pick a number') ;
+if(z>(x+y)){alert('you won');}
+else if(z<(x+y)){alert('you lost');} else{alert('nothing');} 
 
 
 
