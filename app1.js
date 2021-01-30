7b9ac99f8710d7243993a49c04c22938075640a1
KhaledBassam424 <khaleddardour427@gmail.com>
2021-01-30 20:14:22 +0200

    new change

---
+++ app1.js
@@ -0,0 +1,31 @@
+var mohammed = '3'
+var husam = '4'
+console.log(mohammed==husam)
+var username = prompt('what is your name')
+alert('HI '+ username)
+var weight=prompt('what is your weight')
+if(weight>80){alert('go instantly to exercises');}else{alert('you are healthy');}
+var order= prompt('what is your order','Place your order here')
+for(var i=1; i<=3; i++){if(order!=='fruit' || order!=='vegetables' || order!=='crispy chicken'){order= prompt('change you order to a healthier thing','get 10% discount');}
+}
+    
+   
+
+alert('anyways');
+var serviceRate= prompt('how do you rate our service?');
+if(serviceRate==='Excellent'){alert('thanks for visiting us');}
+else if(serviceRate==='good'){
+    var mainIssue= prompt('what was the main issue with the food');
+alert('we got the problem which is '+ mainIssue);}
+else{alert('please fill up the following form');
+Document.write('<img src="http://www.editableforms.com/wp-content/uploads/2013/03/Complaint-Form-Template-post1.png">');}
+
+
+
+
+
+
+
+
+
+document.write('<img src="https://tse2.mm.bing.net/th?id=OIP.wm2cdiu3EhiUBv5ipLRYAQHaDR&pid=Api&P=0&w=383&h=170">')
